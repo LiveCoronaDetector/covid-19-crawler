61bcd2f2dd201826584768553d69490a61852817
dlems <5959ed@gmail.com>
2020-02-26 22:11:16 +0900

    Merge testjson.py and CCCounter.py

--- CCCounter.py
+++ CCCounter.py
@@ -5,6 +5,7 @@
 import re
 import requests
 from bs4 import BeautifulSoup
+from utils import save_json, load_json
 
 
 def postprocess(cc, recovered, dead):
@@ -13,24 +14,32 @@ def postprocess(cc, recovered, dead):
     Args:
         cc: 확진환자수 크롤링한 결과
         recovered: 격리해제수 크롤링한 결과
-        dead: 확진환자수 사망자수 결과
+        dead: 사망자수 크롤링한 결과
 
     Returns:
         후처리된 결과
     """
-    cc = int(cc.replace(',', ''))
-    recovered = int(recovered.replace(',', ''))
-    dead = int(dead.replace(',', ''))
+    cc = cc.replace(',', '').strip()
+    recovered = recovered.replace(',', '').strip()
+    dead = dead.replace(',', '').strip()
+
+    if cc:
+        cc = int(cc)
+    if recovered:
+        recovered = int(recovered)
+    if dead:
+        dead = int(dead)
+
     return cc, recovered, dead
 
 
 def KCDC():
-    """질병관리본부에서 확진환자수, 격리해제수, 사망자수 크롤링
+    """질병관리본부에서 국내 확진환자수, 격리해제수, 사망자수 크롤링
 
     Returns:
-        (int) cc : 확진환자수
-        (int) recoverd : 격리해제수
-        (int) dead : 사망자수
+        (int) cc: 국내 확진환자수
+        (int) recoverd: 국내 격리해제수
+        (int) dead: 국내 사망자수
     """
     html = requests.get("http://ncov.mohw.go.kr/index_main.jsp").text
     soup = BeautifulSoup(html, "html.parser")
@@ -45,37 +54,67 @@ def KCDC():
 
 
 def worldOmeter():
-    """worldOmeter에서 확진환자수, 격리해제수, 사망자수 크롤링
+    """worldOmeter에서 세계 확진환자수, 격리해제수, 사망자수 크롤링
+
+    크롤링 한 결과를 worldmarker.js애 저장
 
     Returns:
-        (int) cc : 확진환자수
-        (int) recoverd : 격리해제수
-        (int) dead : 사망자수
+        (int) cc: 국내 확진환자수
+        (int) recoverd: 국내 격리해제수
+        (int) dead: 국내 사망자수
     """
     html = requests.get("https://www.worldometers.info/coronavirus/").text
     soup = BeautifulSoup(html, "html.parser")
     data = soup.select("#table3 > tbody > tr")
 
+    worldmarker = load_json("./worldmarker.json")
+
+    # 가지고 있던 데이터인지 확인하기 위해
+    cr_cuntries = []
+    for wma in worldmarker:
+        cr_cuntries.extend(wma["Name_cr"])
+
     for datum in data:
         country = datum.find_all("td")[0].text.strip()
-        korea = ["S. Korea", "South Korea"]
-        if country in korea:
-            country_info = datum.find_all('td')
-            cc = country_info[1].text
-            dead = country_info[3].text
-            recovered = country_info[5].text
-            cc, recovered, dead = postprocess(cc, recovered, dead)
-            return [cc, recovered, dead]
-    return None
+        cc = datum.find_all("td")[1].text.strip()
+        dead = datum.find_all("td")[3].text.strip()
+        recovered = datum.find_all("td")[5].text.strip()
+        cc, recovered, dead = postprocess(cc, recovered, dead)
+
+        if country not in cr_cuntries:
+            new = {
+                "Name_cr": [country],
+                "확진자수": cc,
+                "사망자수": dead,
+                "완치자수": recovered
+            }
+            worldmarker.append(new)
+        else:
+            for wm in worldmarker:
+                if country in wm["Name_cr"]:
+                    wm["확진자수"] = cc
+                    wm["사망자수"] = recovered
+                    wm["완치자수"] = dead
+
+                    if "S. Korea" in wm["Name_cr"]:
+                        korea = wm
+
+    save_json(worldmarker, "./worldmarker.json")
+    save_json(worldmarker, "./worldmarker.js")
+
+    cc = korea["확진자수"]
+    recovered = korea["사망자수"]
+    dead = korea["완치자수"]
+    return [cc, recovered, dead]
 
 
 def namuWiki():
-    """나무위키에서 확진환자수, 격리해제수, 사망자수 크롤링
+    """나무위키에서 국내 확진환자수, 격리해제수, 사망자수 크롤링
 
     Returns:
-        (int) cc : 확진환자수
-        (int) recoverd : 격리해제수
-        (int) dead : 사망자수
+        (int) cc: 국내 확진환자수
+        (int) recoverd: 국내 격리해제수
+        (int) dead: 국내 사망자수
     """
     html = requests.get("https://namu.wiki/w/%EC%8B%A0%EC%A2%85%20%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4%EA%B0%90%EC%97%BC%EC%A6%9D").text
     soup = BeautifulSoup(html, "html.parser")
--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # Confirmed Case Counter
-신종 코로나 바이러스의 확진자 정보 업데이트가 질병관리본부(KCDC)의 언론 공개로 공유되고 있어, WHO
-나 일부 사이트만을 이용하여 국내 확진자의 수를 정확히 파악하는 것에 어려움이 있습니다.
-
-[코로나맵](http://livecorona.co.kr/) 사이트에 빠르게 확진자 수를 반영하기 위해, KCDC
-와 여러 사이트에서 확진자 수를 크롤링하여 정확한 확진자 수를 반영합니다.
-
-[KCDC](http://ncov.mohw.go.kr/index_main.jsp)에서의 크롤링이 실패해도, [worldometer
-](https://www.worldometers.info/coronavirus/)
-와 [나무위키](https://namu.wiki/w/%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4%EA%B0%90%EC%97%BC%EC%A6%9D-19?from=%EC%8B%A0%EC%A2%85%20%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4%EA%B0%90%EC%97%BC%EC%A6%9D)의 확진자 데이터를 가져와 반영합니다.
+[코로나맵](http://livecorona.co.kr/) 사이트에 빠르게 확진자 수를 반영하기 위해, [KCDC](http://ncov
+.mohw.go.kr/index_main.jsp), [worldometer](https://www.worldometers.info
+/coronavirus/), [나무위키](https://namu.wiki/w/%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0
+%94%EC%9D%B4%EB%9F%AC%EC%8A%A4%EA%B0%90%EC%97%BC%EC%A6%9D-19?from=%EC%8B%A0
+%EC%A2%85%20%EC%BD%94%EB%A1%9C%EB%82%98%EB%B0%94%EC%9D%B4%EB%9F%AC%EC%8A%A4
+%EA%B0%90%EC%97%BC%EC%A6%9D)의 데이터를 가져옵니다.
 
 <br>
 
@@ -31,3 +28,6 @@ conda env create -f requirements.yaml
         - `namu()` → `namuWiki()`
     - `CCCounter.py`에 `doTry()` 삭제하고, `main()`에 코드로 삽임
     - `CCCounter.py`에 주석 추가 & 코드 정리
+- **2020.02.26**
+    - `testjson.py`과 `CCCounter.py` 병합 (세계정보 크롤링)
+    - `utils.py` 추가
